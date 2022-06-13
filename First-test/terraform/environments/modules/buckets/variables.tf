1f4a4219afc4dc3b344f9da16859b9d306c75879
Amey-khandalkar <khandalkaramey@gmail.com>
2022-06-13 18:51:10 +0530

    first commit

---
+++ First-test/terraform/environments/modules/buckets/variables.tf
@@ -0,0 +1,10 @@
+variable "project_id" {
+  type = string
+}
+variable "names" {
+  type = list(string)
+}
+variable "prefix" {
+  type = string
+}
+
---
+++ First-test/terraform/environments/root/backend.tf
@@ -0,0 +1,5 @@
+terraform {
+  backend "gcs" {
+    bucket = "test1-state"
+  }
+}
